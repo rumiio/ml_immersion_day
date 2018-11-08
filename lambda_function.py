bef30d39c3f84c010ec2939528c61aff8ac468c0
Rumi <32401858+rumiio@users.noreply.github.com>
2018-11-08 10:18:08 -0800

    Update lambda_function.py
    added inline comments

--- lambda_function.py
+++ lambda_function.py
@@ -2,19 +2,23 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
+    # create a client object
     client = boto3.client('rekognition')
+    
+    # replace <YOUR_BUCKET_NAME> with your bucket name and <YOUR_IMAGE_NAME>' with the name of your image
     response = client.detect_labels(
         Image={
             'S3Object': {
-                'Bucket': 'aiml-rumi',
-                'Name': 'ML Immersion Day/birthday_party1.jpeg'
+                'Bucket': '<YOUR_BUCKET_NAME>',
+                'Name': '<YOUR_IMAGE_NAME>'
             }
         })
     
+    # conver the response dictionary object to JSON
     response_str = json.dumps(response)
     response_json = json.loads(response_str)
     
+    # iterate through the JSON to extract what you want to see
     for item in response_json['Labels']:
         if item['Confidence'] >= 80:
             print(item['Name'] + ': Confidence level is ' + str("{0:.1f}%".format(item['Confidence'])))
